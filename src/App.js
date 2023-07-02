7582d148eca36deb72689eeea29b9945a158131b
Dev Shah <devshah120902@gmail.com>
2023-07-03 00:07:10 +0530

    Infinite Loop Added & Some Bug Fix

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import News from "./components/News";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
+  pagesize=9
+  apikey=process.env.REACT_APP_NEWS_API;
   render() {
     return (
       <div>
         <BrowserRouter>
           <NavBar />
           <Routes>
-          <Route path="/" element={<News key="home" pageSize={9} country="in" category="general" news="Top Headlines" />}/>
-          <Route path="/business" element={<News key="business" pageSize={9} country="in" category="business" news="Top Business News" />}/>
-          <Route path="/entertainment" element={<News key="entertainment" pageSize={9} country="in" category="entertainment" news="Top Entertainment News" />}/>
-          <Route path="/general" element={<News key="general" pageSize={9} country="in" category="general" news="Top General News" />}/>
-          <Route path="/health" element={<News key="health" pageSize={9} country="in" category="health" news="Top Health News" />}/>
-          <Route path="/science" element={<News key="science" pageSize={9} country="in" category="science" news="Top Science News" />}/>
-          <Route path="/sports" element={<News key="sports" pageSize={9} country="in" category="sports" news="Top Sports News" />}/>
-          <Route path="/technology" element={<News key="technology" pageSize={9} country="in" category="technology" news="Top Technology News" />}/>
+          <Route path="/" element={<News key="home" pageSize={this.pagesize} country="in" category="general" news="Top Headlines" api={this.apikey}/>}/>
+          <Route path="/business" element={<News key="business" pageSize={this.pagesize} country="in" category="business" news="Top Business News"api={this.apikey} />}/>
+          <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pagesize} country="in" category="entertainment" news="Top Entertainment News"api={this.apikey} />}/>
+          <Route path="/general" element={<News key="general" pageSize={this.pagesize} country="in" category="general" news="Top General News"api={this.apikey} />}/>
+          <Route path="/health" element={<News key="health" pageSize={this.pagesize} country="in" category="health" news="Top Health News"api={this.apikey} />}/>
+          <Route path="/science" element={<News key="science" pageSize={this.pagesize} country="in" category="science" news="Top Science News"api={this.apikey} />}/>
+          <Route path="/sports" element={<News key="sports" pageSize={this.pagesize} country="in" category="sports" news="Top Sports News"api={this.apikey} />}/>
+          <Route path="/technology" element={<News key="technology" pageSize={this.pagesize} country="in" category="technology" news="Top Technology News"api={this.apikey} />}/>
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
     country: "in",
     pageSize: 9,
     category: "general",
-    news:"Top headlines"
+    news: "Top headlines",
   };
 
   static propTypes = {
     country: PropTypes.string,
     pagesize: PropTypes.number,
     category: PropTypes.string,
-    news:PropTypes.string,
+    news: PropTypes.string,
   };
 
   constructor() {
@@ -24,101 +25,87 @@ export class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults:0
     };
   }
+  fetchMoreData = async () => {  
+        this.setState({page: this.state.page + 1})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults
+        })
+      };
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=59c87cf13b2344c88dc685b0ba58ec5f&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
+    let parsedData = await data.json()
     this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-  }
-
-  handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=59c87cf13b2344c88dc685b0ba58ec5f&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
-  };
-
-  handleNextClick = async () => {
-    console.log("Next");
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=59c87cf13b2344c88dc685b0ba58ec5f&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
         articles: parsedData.articles,
-        loading: false,
-      });
-    }
-  };
+        totalResults: parsedData.totalResults,
+    })
+
+}
+async componentDidMount() {
+    this.updateNews();
+}
+
+//   handlePrevClick = async () => {
+//     this.setState({ page: this.state.page - 1 });
+//     this.updateNews();
+//   };
+
+//   handleNextClick = async () => {
+//     this.setState({ page: this.state.page + 1 });
+//     this.updateNews();
+//   };
 
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{ margin: "20px 0px" }}>
           Today's {this.props.news}
         </h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 60) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 90)
-                        : element.title
-                    }
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://static01.nyt.com/images/2023/06/27/science/27SCI-problemofthrees/27SCI-problemofthrees-facebookJumbo.jpg"
-                    }
-                    newsUrl={element.url}
-                    author={element.author?element.author:"Anonymous"}
-                    time={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+        //   loader={<Spinner/>}
+        >
+          <div className="container">
+            <div className="row">
+              {!this.state.loading &&
+                this.state.articles.map((element) => {
+                  return (
+                    <div className="col-md-4" key={element.url}>
+                      <NewsItem
+                        title={element.title ? element.title.slice(0, 60) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 90)
+                            : element.title
+                        }
+                        imageUrl={
+                          element.urlToImage
+                            ? element.urlToImage
+                            : "https://static01.nyt.com/images/2023/06/27/science/27SCI-problemofthrees/27SCI-problemofthrees-facebookJumbo.jpg"
+                        }
+                        newsUrl={element.url}
+                        author={element.author ? element.author : "Anonymous"}
+                        time={element.publishedAt}
+                        source={element.source.name}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -139,8 +126,8 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
